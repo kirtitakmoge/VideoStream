7fe24e4dd39dd1b4bd6df5ffbc52b6cfbe4b97d2
kirtitakmoge <121674825+kirtitakmoge@users.noreply.github.com>
2024-05-03 12:24:13 +0530

    Patient

--- frontend/src/components/PatientData.js
+++ frontend/src/components/PatientData.js
@@ -1,28 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { useAuth } from './AuthContext';
+import React, { useState, useEffect } from "react";
+import { useAuth } from "./AuthContext";
 
 const PatientData = () => {
   const [patients, setPatients] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
- const {user}=useAuth();
- const token=localStorage.getItem("token");
+  const { user } = useAuth();
+  const token = localStorage.getItem("token");
   useEffect(() => {
     const fetchPatients = async () => {
       try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/patient/getAllPatientByDepartmentId/${user.departmentId}`, {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/api/patient/getAllPatientByDepartmentId/${user.departmentId}`,
+          {
             method: "GET",
             headers: {
-                "Content-Type": "application/json",
-                 "Authorization": `Bearer ${token}` // Include the token in the Authorization header
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`, // Include the token in the Authorization header
             },
-        }); // Replace with your API endpoint
+          }
+        ); // Replace with your API endpoint
         const data = await response.json();
         setPatients(data);
         console.log(data);
         setLoading(false);
       } catch (error) {
-        setError('Error fetching patients');
+        setError("Error fetching patients");
         setLoading(false);
       }
     };
@@ -45,28 +48,31 @@ const PatientData = () => {
               <th className="px-4 py-2">Gender</th>
               <th className="px-4 py-2">Email</th>
               <th className="px-4 py-2">Address</th>
-              
+
               <th className="px-4 py-2">File</th>
             </tr>
           </thead>
           <tbody>
-            {patients.map(patient => (
+            {patients.map((patient) => (
               <tr key={patient._id}>
                 <td className="border px-4 py-2">{patient.firstname}</td>
                 <td className="border px-4 py-2">{patient.age}</td>
                 <td className="border px-4 py-2">{patient.gender}</td>
                 <td className="border px-4 py-2">{patient.email}</td>
                 <td className="border px-4 py-2">{patient.address}</td>
-                
+
                 <td className="border px-4 py-2">
-                <ul>
-                            {patient.patientcontentId && patient.patientcontentId.link.map((link, index) => (
-                                <li key={index}>
-                                
-                                    <div>Object Key: {link.objectKey}</div>
-                                </li>
-                            ))}
-                        </ul>
+                  {patient.patientcontentId ? (
+                    <ul>
+                      {patient.patientcontentId.link.map((link, index) => (
+                        <li key={index}>
+                          <div>Object Key: {link.objectKey}</div>
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <div>File is not shared</div>
+                  )}
                 </td>
               </tr>
             ))}
@@ -75,6 +81,5 @@ const PatientData = () => {
       )}
     </div>
   );
-  
-}
+};
 export default PatientData;
